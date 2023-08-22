508006aaffabfb15921d9087b03f2c2ce8ebfe97
monstblu <wdsrs020808@gmail.com>
2023-08-22 12:54:14 -0700

    08/22/2023 1:00 PM

--- cuckoo_hash.py
+++ cuckoo_hash.py
@@ -12,43 +12,80 @@ def random_key(length=8):
 class CuckooHashTable:
     def __init__(self, size=128):
         self.size = size
-        self.table = [None] * (size * 4)  # Create a table with size * 4 slots
-        self.MAX_RELOCATIONS = 32  # Reduced for quick observation
+        # Each table has 'size' buckets, and each bucket can store up to 4 items
+        self.table1 = [None] * size
+        self.table2 = [None] * size
+        for i in range(size):
+            self.table1[i] = [None] * 4
+            self.table2[i] = [None] * 4
+        self.MAX_RELOCATIONS = 32 # Reduced for quick observation
 
     def _hashes(self, key):
         h1 = mmh3.hash(key, 1) % self.size
         h2 = mmh3.hash(key, 2) % self.size
-        return [h1, h1 + self.size, h2, h2 + self.size]  # Each hash points to two buckets
+        return h1, h2
 
     def insert(self, key, value):
         relocations = 0
         for _ in range(self.MAX_RELOCATIONS):
             relocations += 1
-            for index in self._hashes(key):
-                if self.table[index] is None:
-                    self.table[index] = (key, value)
-                    return True, relocations  # Return number of relocations
-                key, value, self.table[index] = self.table[index][0], self.table[index][1], (key, value)
-        
+            
+            h1, h2 = self._hashes(key)
+            # Try inserting in table1
+            for slot in range(4):
+                if self.table1[h1][slot] is None:
+                    self.table1[h1][slot] = (key, value)
+                    return True, relocations
+
+            # Try inserting in table2
+            for slot in range(4):
+                if self.table2[h2][slot] is None:
+                    self.table2[h2][slot] = (key, value)
+                    return True, relocations
+
+            # Displace a random item from table1
+            slot = random.choice(range(4))
+            key, value, self.table1[h1][slot] = self.table1[h1][slot][0], self.table1[h1][slot][1], (key, value)
+
         print("Failed to insert after maximum relocations. Consider resizing or rehashing.")
         return False, relocations
 
     def get(self, key):
-        for index in self._hashes(key):
-            if self.table[index] and self.table[index][0] == key:
-                return self.table[index][1]
+        h1, h2 = self._hashes(key)
+        
+        # Check table1
+        for slot in self.table1[h1]:
+            if slot and slot[0] == key:
+                return slot[1]
+        
+        # Check table2
+        for slot in self.table2[h2]:
+            if slot and slot[0] == key:
+                return slot[1]
+
         return None
 
     def delete(self, key):
-        for index in self._hashes(key):
-            if self.table[index] and self.table[index][0] == key:
-                self.table[index] = None
+        h1, h2 = self._hashes(key)
+        
+        # Check table1
+        for i in range(4):
+            if self.table1[h1][i] and self.table1[h1][i][0] == key:
+                self.table1[h1][i] = None
                 return True
+        
+        # Check table2
+        for i in range(4):
+            if self.table2[h2][i] and self.table2[h2][i][0] == key:
+                self.table2[h2][i] = None
+                return True
+
         return False
     
     def load_factor(self):
-        filled_slots = sum(1 for slot in self.table if slot is not None)
-        return filled_slots / (self.size * 4)
+        filled_slots = sum(1 for bucket in self.table1 for slot in bucket if slot is not None)
+        filled_slots += sum(1 for bucket in self.table2 for slot in bucket if slot is not None)
+        return filled_slots / (self.size * 4 * 2)
 
 if __name__ == "__main__":
     import sys

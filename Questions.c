018c95f7471f07dceddc07f3701c52009f243bc4
Blue Monster <73154603+MonsterBlue01@users.noreply.github.com>
2023-08-28 20:29:08 +0000

    08/28/2023 1:30 PM

--- Questions.c
+++ Questions.c
@@ -1,1 +1,2 @@
-// 1. Since 2-4 cuckoo hashes generally have countermeasures against infinite loops, the meaning of "low count, high load" should be...
+// 1. En la actualidad, solo podemos encontrar uno por uno y no hay forma de atribuir los ocho elementos aplicables que son aplicables a situaciones comunes. Esto puede ser un problema de NP Hard.
+// 2. ¿Realmente necesitas nueve elementos para formar un bucle infinito? Porque si solo fueran ocho cabrían todos en el picadillo de cuco.

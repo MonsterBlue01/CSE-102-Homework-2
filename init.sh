434f057ce79474592959bd0fe5d8cf38d7b5bab3
Dongjing Wang <monstblu@proton.me>
2024-04-02 14:44:20 -0700

    04/02/2024 2:45 PM

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
+# Check if there is already a container named "mydb" running
+if [ "$(docker ps -aq -f name=mydb)" ]; then
+    # If the container already exists, stop and delete it first
+    docker stop mydb
+    docker rm mydb
+fi
+
 docker build -t postgres-sample-db .
 docker run -d --name mydb -p 5432:5432 postgres-sample-db
-sleep 3
+sleep 8
 psql -h localhost -p 5432 -U postgres postgres
